54f0e914d20da756bf5d5e4f01fdf5b12cbc7d5a
Pranesh Vinayak Gaikwad <pranesh2008517@gmail.com>
2026-04-25 13:58:20 +0530

    Adding dashboard get method

--- cts/src/main/java/com/cts/pages/CtsDashboardPage.java
+++ cts/src/main/java/com/cts/pages/CtsDashboardPage.java
@@ -3,18 +3,25 @@ package com.cts.pages;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
-import org.athena.BasePage;
+
+import java.util.List;
 
 public class CtsDashboardPage extends CtsBasePage {
 
     private Page page;
 
     private Locator logoutButton;
+    private Locator collapsibleMenuItems;
+    private Locator collapsibleMenuItemHeaders;
+    private Locator menuItemList;
 
     public CtsDashboardPage(Page page) {
         super(page);
         this.page = page;
         this.logoutButton = page.locator(".header svg").nth(2);
+        this.collapsibleMenuItems = page.locator(".menu-collapse");
+        this.collapsibleMenuItemHeaders = page.locator(".menu-collapse-item .items-center");
+        this.menuItemList = page.locator("ul .menu-item");
     }
 
     @Step("Verify CTS Dashboard is displayed")
@@ -32,5 +39,14 @@ public class CtsDashboardPage extends CtsBasePage {
         attachScreenshot(page, "Logout from CTS");
 
         return new LoginPage(page);
+    }
+
+    public List<String> getCollapsibleMenuItems(String menuItemHeader) {
+        for (Locator eachMenu: collapsibleMenuItems.all()) {
+            if (eachMenu.locator(collapsibleMenuItemHeaders).textContent().equals(menuItemHeader)) {
+                return eachMenu.locator(menuItemList).allTextContents();
+            }
+        }
+        return null;
     }
 }
